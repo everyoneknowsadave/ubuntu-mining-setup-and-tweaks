accb4f42d3636fa12e92ffc1dbdf0c39f2795aae
Ecommerce Sussex <davidhazeldenuk@gmail.com>
2019-05-02 01:41:20 +0100

    Update miner_config_eth.sh

--- ethminer/bin/miner_config_eth.sh
+++ ethminer/bin/miner_config_eth.sh
@@ -3,4 +3,4 @@
 #setx GPU_USE_SYNC_OBJECTS 1
 #setx GPU_MAX_ALLOC_PERCENT 100
 #setx GPU_SINGLE_ALLOC_PERCENT 100
-./ethminer --farm-recheck 200 -U --cuda-devices 0 1 2 3 -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O 0x266e19fbf9ee26adc24b4bd3dd53de8c2a705999.rig001
+./ethminer --farm-recheck 200 -U --cuda-devices 0 1 2 3 -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O 0x266e19fbf9ee26adc24b4bd3dd53de8c2a70599912.rig001
